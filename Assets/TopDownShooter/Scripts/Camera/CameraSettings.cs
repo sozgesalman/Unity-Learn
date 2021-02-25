953180a87185e82a57ae5c53e4a4233a23ba8ad6
Ozge Salman <sozgesalman@gmail.com>
2021-02-25 23:08:27 +0300

    --added: CameraSettings & CameraController

---
+++ Assets/TopDownShooter/Scripts/Camera/CameraSettings.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDownShooter
+{
+    [CreateAssetMenu(menuName = "TopDown Shooter/Camera/Camera Settings")]
+    public class CameraSettings : ScriptableObject
+    {
+        [Header("Rotation")]
+        [SerializeField] private float _rotationLerpSpeed = 1;
+        public float RotationLerpSpeed { get { return _rotationLerpSpeed; } }
+
+        [Header("Position")]
+        [SerializeField] private Vector3 _positionOffSet;
+        public Vector3 PositionOffSet { get { return _positionOffSet; } }
+
+        [SerializeField] private float _positionLerp = 1;
+        public float PositionLerb { get { return _positionLerp; } }
+    }
+}
