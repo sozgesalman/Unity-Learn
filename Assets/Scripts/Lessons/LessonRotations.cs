8fe6283994175a77394919987e514b43a080c8fb
Ozge Salman <sozgesalman@gmail.com>
2021-02-25 16:58:43 +0300

    --created:RotationScript, ScriptableObject

---
+++ Assets/Scripts/Lessons/LessonRotations.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lessons
+{
+    public class LessonRotations : MonoBehaviour
+    {
+        ///[SerializeField] private float _speed = 1;
+        [SerializeField] private RotateSettings _rotateSettings;
+        void Update()
+        {
+            transform.Rotate(Vector3.up, _rotateSettings.Speed * Time.deltaTime);
+        }
+    }
+}
