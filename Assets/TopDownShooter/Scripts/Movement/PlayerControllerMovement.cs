ac92f5db4f985e1d20993211822e264e8d69e045
Ozge Salman <sozgesalman@gmail.com>
2021-02-27 02:24:11 +0300

    --add: Player movement

---
+++ Assets/TopDownShooter/Scripts/Movement/PlayerControllerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TopDownShooter.PlayerInput;
+using UnityEngine;
+
+namespace TopDownShooter.PlayerMovement
+{
+    public class PlayerControllerMovement : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody _rigidBody;
+        [SerializeField] private InputData _inputData;
+        [SerializeField] private PlayerMovementSettings _playerMovementSettings;
+        private void Update()
+        {
+            _rigidBody.MovePosition(_rigidBody.position + ( _rigidBody.transform.forward * _inputData.Vertical * _playerMovementSettings.VerticalSpeed));
+            _rigidBody.MovePosition(_rigidBody.position + ( _rigidBody.transform.right * _inputData.Horizontal * _playerMovementSettings.HorizontalSpeed));
+        }
+    }
+}
