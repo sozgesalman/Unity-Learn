369fd3215c9cfca4ce1b6e9b716db504c7c8de94
Ozge Salman <sozgesalman@gmail.com>
2021-02-27 02:25:17 +0300

    --add:Input operations

---
+++ Assets/TopDownShooter/Scripts/Input/InputData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace TopDownShooter.PlayerInput
+{
+    [CreateAssetMenu(menuName = "TopDown Shooter/Input/Input Data")]
+    public class InputData : ScriptableObject
+    {
+        public float Horizontal;
+        public float Vertical;
+    }
+}
