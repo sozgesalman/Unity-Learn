64f41eecc451c3c310f436f56df86644c976e8d1
Ozge Salman <sozgesalman@gmail.com>
2021-03-02 01:15:15 +0300

    --refactor: Rotation control with mouse rotation

--- Assets/TopDownShooter/Scripts/Input/InputManager.cs
+++ Assets/TopDownShooter/Scripts/Input/InputManager.cs
@@ -7,11 +7,16 @@ namespace TopDownShooter.PlayerInput
     public class InputManager : MonoBehaviour
     {
         [SerializeField] private InputData _inputData;
+        [SerializeField] private InputData _rotationInputData;
+        private Vector3 _LastMousePosition;
 
         private void Update()
         {
             _inputData.Horizontal = Input.GetAxis("Horizontal") ;
             _inputData.Vertical = Input.GetAxis("Vertical");
+
+            _rotationInputData.Horizontal = (Input.mousePosition.x - _LastMousePosition.x);
+            _LastMousePosition = Input.mousePosition;
 
         }
     }
---
+++ Assets/TopDownShooter/Scripts/Movement/PlayerRotationController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TopDownShooter.PlayerInput;
+
+
+public class PlayerRotationController : MonoBehaviour
+{
+
+    [SerializeField] private InputData _inputData;
+    [SerializeField] private Transform _tower;
+
+    private void Update()
+    {
+        _tower.transform.Rotate(0, _inputData.Horizontal, 0, Space.Self);
+    }
+}
