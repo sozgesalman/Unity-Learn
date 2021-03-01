7247f066fe9cb4c8d9d6cd5e72f4ddcc092d3fb8
Ozge Salman <sozgesalman@gmail.com>
2021-03-02 01:12:37 +0300

    --refactor: Stat system for trying (it's not useful)

---
+++ Assets/TopDownShooter/Scripts/Stat/HealthStat.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthStat : MonoBehaviour
+{
+    private float Health = 100;
+    public void Hit(float damage)
+    {
+        if(Health <= 0)
+        {
+            Debug.Log("Health zero.");
+        }
+        else
+        {
+            Debug.Log("Current Health Account: " +Health);
+        }
+    }
+}
