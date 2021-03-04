6dc2c2e321c373db37fbe3af517d978e27fbc0e1
Ozge Salman <sozgesalman@gmail.com>
2021-03-04 23:47:40 +0300

    --fix: Player movement key control

---
+++ Assets/TopDownShooter/Scripts/PlayerControl/WheelRotation/WheelRotationManagement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TopDownShooter.PlayerInput;
+
+namespace TopDownShooter.PlayerControls {
+    public class WheelRotationManagement : MonoBehaviour
+    {
+         [SerializeField] private Rigidbody _rigidBody;
+        [SerializeField] private InputData _rotationInput;
+        [SerializeField] private Transform _wheelTransform;
+        [SerializeField] private WheelRotationSettings _wheelRotationSettings;
+
+        private void Update()
+        {
+            _wheelTransform.transform.Rotate(_rotationInput.Vertical * _wheelRotationSettings.RotationSpeed, 0, 0, Space.Self);
+            //_wheelTransform.transform.Rotate(Vector3.up*_wheelRotationSettings.RotationSpeed, _wheelRotationSettings.RotationAngle);
+           
+        }
+    } 
+}
---
+++ Assets/TopDownShooter/Scripts/PlayerControl/WheelRotation/WheelRotationSettings.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDownShooter.PlayerControls
+{
+    [CreateAssetMenu(menuName = "TopDown Shooter/Player/Wheel Rotation Settings")]
+    public class WheelRotationSettings : ScriptableObject
+    {
+        public float RotationSpeed = 1;
+        //public float RotationAngle = 30;
+
+
+
+    }
+}
