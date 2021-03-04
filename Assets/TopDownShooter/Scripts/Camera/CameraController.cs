8ef49e7700c7e9c4d1b28f7d56b3a29cf7e54bdb
Ozge Salman <sozgesalman@gmail.com>
2021-03-04 20:04:08 +0300

    --fix: camera rotation controller

--- Assets/TopDownShooter/Scripts/Camera/CameraController.cs
+++ Assets/TopDownShooter/Scripts/Camera/CameraController.cs
@@ -36,12 +36,13 @@ namespace TopDownShooter.Camera
 
         private void CameraMovementFollow()
         {
-            Vector3 offset = (_cameraTransform.right * _cameraSettings.PositionOffSet.x) + (_cameraTransform.up * _cameraSettings.PositionOffSet.y) +
-                (_cameraTransform.forward * _cameraSettings.PositionOffSet.z);
-            _cameraTransform.position = Vector3.Lerp(_cameraTransform.position,_targetTransform.position + offset,
-                Time.deltaTime * _cameraSettings.PositionLerb);               
-            
+            //Vector3 offset = (_cameraTransform.right * _cameraSettings.PositionOffSet.x) + (_cameraTransform.up * _cameraSettings.PositionOffSet.y) +
+            //    (_cameraTransform.forward * _cameraSettings.PositionOffSet.z);
+            //_cameraTransform.position = Vector3.Lerp(_cameraTransform.position,_targetTransform.position + offset,
+            //    Time.deltaTime * _cameraSettings.PositionLerb);               
 
+            ///////////****** yukardaki kod aşağıdakiyle aynı işi yapıyor, daha iyi yapıyor.
+            _cameraTransform.localPosition = _cameraSettings.PositionOffSet;
         }
 
         
