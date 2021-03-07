cec3927e422d6556219d25a1dc57b40f2c1667d5
Ozge Salman <sozgesalman@gmail.com>
2021-03-07 23:21:10 +0300

    --feat: The beginning of Abstract

---
+++ Assets/TopDownShooter/Scripts/Inventory/PlayerInventoryCannonItemData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDownShooter.Inventory
+{
+    [CreateAssetMenu(menuName = "TopDown Shooter/Inventory/Player Inventory Cannon Item Data")]
+
+    public class PlayerInventoryCannonItemData : AbstractPlayerInventoryItemData
+    {
+        public override void CreateIntoInventory(PlayerInventoryController targetPlayerInventory)
+        {
+            Debug.Log("THIS IS INVENTORY CANNON ITEM DATA");
+        }
+    }
+}
---
+++ Assets/TopDownShooter/Scripts/Inventory/PlayerInventoryController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDownShooter.Inventory
+{
+    public class PlayerInventoryController : MonoBehaviour
+    {
+
+        [SerializeField] private AbstractPlayerInventoryItemData[] _inventoryItemDataArray;
+
+
+        private void Start()
+        {
+            IntializeInventory(_inventoryItemDataArray);   
+        }
+
+        public void IntializeInventory(AbstractPlayerInventoryItemData[] InventoryItemDataArray)
+        {
+            for(int i = 0; i< InventoryItemDataArray.Length; i++)
+            {
+                InventoryItemDataArray[i].CreateIntoInventory(this);
+            }
+        }
+
+    }
+}
